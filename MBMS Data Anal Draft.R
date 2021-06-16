2300ca3ea3243206e7550ec2e00e6e54748d62df
Zoe Crawley <zoecrawley@MacBook-Pro-98.local>
2021-06-16 09:22:47 -0500

    filtered missing data and check data table

--- MBMS Data Anal Draft.R
+++ MBMS Data Anal Draft.R
@@ -1,9 +1,26 @@
 library(tibble)
 library(dplyr)
+library(data.table)
+#practice filters for un-used IDs and Pre-only#
 filter(PrePostData, Record_ID != "12002" & Record_ID != "12009")
 PrePostData %>%
   select(Record_ID, Session, PANAS_Start_Pos, PANAS_Start_Neg, IOS_Pre) %>%
   filter(Record_ID != "12002" & Record_ID != "12009")
+#create new data set with filtered data#
 PreOnlyData_Filter <- PrePostData %>%
   select(Record_ID, Session, PANAS_Start_Pos, PANAS_Start_Neg, IOS_Pre) %>%
   filter(Record_ID != "12002" & Record_ID != "12009")
+#remove missing data#
+PreOnlyData_Filter %>%
+  filter(!is.na(PANAS_Start_Pos),
+         !is.na(PANAS_Start_Neg),
+         !is.na(IOS_Pre))
+PreOnlyData_Filter<- PreOnlyData_Filter %>%
+  filter(!is.na(PANAS_Start_Pos),
+         !is.na(PANAS_Start_Neg),
+         !is.na(IOS_Pre))
+#check data table#
+setDT(PreOnlyData_Filter)
+data.table(PreOnlyData_Filter ,keep.rownames = TRUE)
+
+
